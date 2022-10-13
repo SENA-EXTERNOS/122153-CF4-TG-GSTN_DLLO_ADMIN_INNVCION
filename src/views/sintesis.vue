0d3cc40439b741450ffdc3a7ce89d7f7d7234d1e
Leoska89 <83103793+Leoska89@users.noreply.github.com>
2022-10-13 06:24:56 -0500

    up 1

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,7 +2,17 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-
+    .row.justify-content-center 
+      .col-lg-12
+        .row.mb-5   
+          .col-lg-8   
+            .titulo-sexto.color-acento-botones
+              h3 Gestión del desarrollo administrativo e innovación
+              p.mb-0 Síntesis: Los clientes, el servicio y la comunicación organizacional
+          .col-lg-4
+              figure.mb-5
+                img.img-86(src='@/assets/curso/temas/logo-sena.svg')              
+   
     p.mb-5 A continuación, encontrará una síntesis de las temáticas abordadas en el componente formativo.
 
     .row.justify-content-center
